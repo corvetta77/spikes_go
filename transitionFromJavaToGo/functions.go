35af005794d898764c8c3f8b6be85cdbe4d1b3e1
corvetta77 <szczepan.kurnyta@gmail.com>
2023-03-13 22:45:57 +0100

    Functions and pointers

--- transitionFromJavaToGo/functions.go
+++ transitionFromJavaToGo/functions.go
@@ -17,4 +17,16 @@ func main() {
 	fmt.Printf("add(%v,%v) = %v\n", 5, 4, add(5, 4))
 	fmt.Printf("sub(%v,%v) = %v\n", 5, 4, sub(5, 4))
 
+	x := 3
+
+	negate := func(x *int) {
+		neg := -*x
+		*x = neg
+	}
+
+	fmt.Println(x)
+	fmt.Println(&x)
+
+	negate(&x)
+	fmt.Println(x)
 }
